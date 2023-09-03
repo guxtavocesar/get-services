29abb04d05f640e5f756ca66c3135872a8036b48
guxtavocesar <gclp2004@gmail.com>
2023-09-03 12:00:05 -0300

    Atualização nas cores da página de pesquisa

--- pagina_pesquisa/index.php
+++ pagina_pesquisa/index.php
@@ -123,7 +123,7 @@ include('../logout/verifica_login.php');
         <div class="col-lg-12 my-1 p-5 border border-outline-white rounded-5">
           <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
             <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+            <rect width="100%" height="100%" fill="#C9586C" />
           </svg>
           <div class="col-10" name="informacoes" style="float: right; padding-right: 35%;">
           <div class="field">
