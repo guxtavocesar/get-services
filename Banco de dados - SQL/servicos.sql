8fb39783e6a3f4b04899c13e9562f0083d96a922
guxtavocesar <gclp2004@gmail.com>
2023-09-03 21:48:44 -0300

    Atualização: Adicionado script para inserção de dados na tabela prestador_servicos para testagem rápida da ferramenta de pesquisa

--- Banco de dados - SQL/servicos.sql
+++ Banco de dados - SQL/servicos.sql
@@ -35,3 +35,130 @@ CREATE TABLE prestador_servicos
     ddd CHAR(2)
 );
 
+-- Inserção de dados na tabela prestacao_servicos para teste rápido
+
+INSERT INTO prestador_servicos (
+    tipo_usuario, 
+    senha, 
+    nome, 
+    nasc, 
+    celular, 
+    email, 
+    rg, 
+    cpf, 
+    profissao, 
+    carreira, 
+    cidade, 
+    uf, 
+    ddd) 
+
+VALUES (
+    'prestador',
+    123456,
+    'João Jorge Pinheiro',
+    2000-02-03,
+    997869324,
+    'joaojorge@gmail.com',
+    583214597,
+    54154256859,
+    'Arquiteto',
+    'Arquiteto formado na Fundação Educacional do Município de Assis',
+    'Assis',
+    'SP',
+    18
+);
+
+INSERT INTO prestador_servicos (
+    tipo_usuario, 
+    senha, 
+    nome, 
+    nasc, 
+    celular, 
+    email, 
+    rg, 
+    cpf, 
+    profissao, 
+    carreira, 
+    cidade, 
+    uf, 
+    ddd) 
+
+VALUES (
+    'prestador',
+    123456,
+    'Edson Correa dos Santos',
+    2000-02-03,
+    996584523,
+    'edson@gmail.com',
+    421568951,
+    50154741236,
+    'Designer',
+    'Designer freelancer aberto para projetos',
+    'São Paulo',
+    'SP',
+    11
+);
+
+INSERT INTO prestador_servicos (
+    tipo_usuario, 
+    senha, 
+    nome, 
+    nasc, 
+    celular, 
+    email, 
+    rg, 
+    cpf, 
+    profissao, 
+    carreira, 
+    cidade, 
+    uf, 
+    ddd) 
+
+VALUES (
+    'prestador',
+    123456,
+    'Carlos Ribeiro',
+    2000-02-03,
+    998745698,
+    'carlos@gmail.com',
+    487569632,
+    49969871422,
+    'Contador',
+    'Contador com experiência em mais de 10 empresas de médio porte',
+    'Assis',
+    'SP',
+    18
+);
+
+INSERT INTO prestador_servicos (
+    tipo_usuario, 
+    senha, 
+    nome, 
+    nasc, 
+    celular, 
+    email, 
+    rg, 
+    cpf, 
+    profissao, 
+    carreira, 
+    cidade, 
+    uf, 
+    ddd) 
+
+VALUES (
+    'prestador',
+    123456,
+    'Steve Rogers',
+    1939-07-04,
+    997265472,
+    'steve@gmail.com',
+    603127939,
+    50155054856,
+    'Soldador',
+    'Soldador de caminhões desde antes dos anos 2000',
+    'Assis',
+    'SP',
+    18
+);
+
+
